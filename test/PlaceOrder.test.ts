42d272968c3a6699218e51bc1eba1233fee828a3
Cícero Junior <cicerojunior.cont@gmail.com>
2021-07-30 20:18:42 -0300

    test: criando teste de place order

--- test/PlaceOrder.test.ts
+++ test/PlaceOrder.test.ts
@@ -8,6 +8,20 @@ test("Deve fazer um pedido com 3 itens (com descrição, preço e quantidade)", 
             { description : "Bola", price: 100, quantity: 1},
             { description : "Tênis", price: 100, quantity: 2},
             { description : "Meião", price: 100, quantity: 3},
+        ]
+    }
+    const placeOrder = new PlaceOrder();
+    const output = placeOrder.execute(input);
+    expect(output.total).toBe(600);   
+});
+
+test("Deve fazer um pedido com cupom de desconto (percentual sobre o total do pedido)", ()=> {
+    const input:PlaceOrderInput = {
+        cpf: "487.501.680-88",
+        items: [
+            { description : "Bola", price: 100, quantity: 1},
+            { description : "Tênis", price: 100, quantity: 2},
+            { description : "Meião", price: 100, quantity: 3},
         ],
         coupons: [
             "VALE20",
